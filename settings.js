373bd16bad0ecbae94112a8dba939dfb1ba3d4c0
Samuel Kapito <166576540+Cermuel31@users.noreply.github.com>
2026-04-04 07:28:51 +0200

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'Matrix Bot',
+  author: '‎Mr Samuel Kapito',
+  botName: "Matrix Bot",
+  botOwner: 'Samuel Kapito', // Your name
+  ownerNumber: '265995957560', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "Matrix Bot MD is a multi-device WhatsApp bot built using Baileys, designed for group commands and automating tasks.",
+  version: "3.0.7",
+  updateZipUrl: "https://github.com/Cermuel31/Matrix-Bot-MD/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
